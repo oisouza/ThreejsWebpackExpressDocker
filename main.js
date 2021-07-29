cb2ea198d8f56bea7d4276c979450a90081e12a5
eduardosilva218 <eduardo.silva218@outlook.com>
2021-07-29 00:03:36 -0300

    Updates

--- main.js
+++ main.js
@@ -36,7 +36,37 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
   \*************************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var three__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! three */ \"./node_modules/three/build/three.module.js\");\n/* harmony import */ var _assets_kanna_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../assets/kanna.jpg */ \"./src/assets/kanna.jpg\");\n\r\n\r\n\r\nconst scene = new three__WEBPACK_IMPORTED_MODULE_1__.Scene();\r\nconst camera = new three__WEBPACK_IMPORTED_MODULE_1__.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.1, 1000 );\r\n\r\nconst renderer = new three__WEBPACK_IMPORTED_MODULE_1__.WebGLRenderer();\r\nrenderer.setSize( window.innerWidth, window.innerHeight );\r\ndocument.body.appendChild( renderer.domElement );\r\n\r\nconst texture = new three__WEBPACK_IMPORTED_MODULE_1__.TextureLoader().load( _assets_kanna_jpg__WEBPACK_IMPORTED_MODULE_0__.default );\r\n\r\nconst geometry = new three__WEBPACK_IMPORTED_MODULE_1__.PlaneGeometry();\r\nconst material = new three__WEBPACK_IMPORTED_MODULE_1__.MeshBasicMaterial( { map: texture } );\r\nconst sprite = new three__WEBPACK_IMPORTED_MODULE_1__.Mesh( geometry, material );\r\nscene.add( sprite );\r\n\r\ncamera.position.z = 5;\r\n\r\nconst animate = function () {\r\n    requestAnimationFrame( animate );\r\n\r\n    sprite.rotation.x += 0.01;\r\n    sprite.rotation.y += 0.01;\r\n\r\n    renderer.render( scene, camera );\r\n};\r\n\r\nanimate();\r\n\n\n//# sourceURL=webpack://threejs-webpack-express/./src/js/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var three__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! three */ \"./node_modules/three/build/three.module.js\");\n/* harmony import */ var _assets_kanna_jpg__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../assets/kanna.jpg */ \"./src/assets/kanna.jpg\");\n/* harmony import */ var _modules_cameraScript__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/cameraScript */ \"./src/js/modules/cameraScript.js\");\n/* harmony import */ var _modules_motorGame__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/motorGame */ \"./src/js/modules/motorGame.js\");\n/* harmony import */ var _modules_spriteScript__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/spriteScript */ \"./src/js/modules/spriteScript.js\");\n\r\n\r\n\r\n\r\n\r\n\r\nconst scene = new three__WEBPACK_IMPORTED_MODULE_4__.Scene();\r\n\r\nconst renderer = new three__WEBPACK_IMPORTED_MODULE_4__.WebGLRenderer();\r\nrenderer.setSize( window.innerWidth, window.innerHeight );\r\n\r\nconst objectSprite = new three__WEBPACK_IMPORTED_MODULE_4__.Mesh( \r\n    new three__WEBPACK_IMPORTED_MODULE_4__.PlaneGeometry(), \r\n    new three__WEBPACK_IMPORTED_MODULE_4__.MeshBasicMaterial({ \r\n        map: new three__WEBPACK_IMPORTED_MODULE_4__.TextureLoader().load( _assets_kanna_jpg__WEBPACK_IMPORTED_MODULE_0__.default )\r\n    })\r\n)\r\nconst sprites = [\r\n    new _modules_spriteScript__WEBPACK_IMPORTED_MODULE_3__.spriteScript(\r\n        objectSprite,\r\n        () => {}, \r\n        () => {\r\n            objectSprite.rotation.x += 0.01;\r\n            objectSprite.rotation.y += 0.01;\r\n        })\r\n]\r\n\r\nconst objectCamera = new three__WEBPACK_IMPORTED_MODULE_4__.PerspectiveCamera( 25, window.innerWidth / window.innerHeight, 0.1, 1000 )\r\nconst camera = new _modules_cameraScript__WEBPACK_IMPORTED_MODULE_1__.cameraScript(\r\n    objectCamera,\r\n    () => { \r\n        objectCamera.position.z = 5 \r\n    },\r\n    () => {})\r\n\r\nconst motor = new _modules_motorGame__WEBPACK_IMPORTED_MODULE_2__.motorGame(sprites, scene, camera, renderer)\r\n\r\ndocument.body.appendChild( motor.domElement );\r\n\r\nmotor.load()\r\nmotor.animate()\r\n\n\n//# sourceURL=webpack://threejs-webpack-express/./src/js/index.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/cameraScript.js":
+/*!****************************************!*\
+  !*** ./src/js/modules/cameraScript.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"cameraScript\": () => (/* binding */ cameraScript)\n/* harmony export */ });\n\r\nclass cameraScript {\r\n    threeCamera\r\n    load\r\n    animate\r\n\r\n    constructor(\r\n        threeCamera,\r\n        load = () => {}, \r\n        animate = () => {}){\r\n        this.threeCamera = threeCamera\r\n        this.load = load\r\n        this.animate = animate\r\n    }\r\n}\r\n\r\n\n\n//# sourceURL=webpack://threejs-webpack-express/./src/js/modules/cameraScript.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/motorGame.js":
+/*!*************************************!*\
+  !*** ./src/js/modules/motorGame.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"motorGame\": () => (/* binding */ motorGame)\n/* harmony export */ });\n\r\nclass motorGame {\r\n    $spriteScripts\r\n    $scene\r\n    $cameraScript\r\n    $renderer\r\n    domElement\r\n\r\n    constructor(spriteScripts, scene, cameraScript, renderer){\r\n        this.$setup(spriteScripts, scene, cameraScript, renderer)\r\n    }\r\n\r\n    $setup(spriteScripts, scene, cameraScript, renderer)\r\n    {        \r\n        this.$spriteScripts = spriteScripts\r\n        this.$scene = scene\r\n        this.$cameraScript = cameraScript\r\n        this.$renderer = renderer\r\n        this.domElement = renderer.domElement\r\n    }\r\n\r\n    load() {\r\n        this.$cameraScript.load()\r\n        this.$spriteScripts.forEach(\r\n            spriteScript => \r\n            {\r\n                this.$scene.add(spriteScript.threeMesh);\r\n                spriteScript.load()\r\n            })\r\n    }\r\n\r\n    animate() {\r\n        requestAnimationFrame(this.animate.bind(this));\r\n        this.$spriteScripts.forEach(spriteScript => {\r\n            spriteScript.animate()\r\n            this.$cameraScript.animate()\r\n            this.$render()\r\n        });\r\n    }\r\n    \r\n    $render() {\r\n        this.$renderer.render(this.$scene, this.$cameraScript.threeCamera);\r\n    } \r\n}\r\n\r\n\n\n//# sourceURL=webpack://threejs-webpack-express/./src/js/modules/motorGame.js?");
+
+/***/ }),
+
+/***/ "./src/js/modules/spriteScript.js":
+/*!****************************************!*\
+  !*** ./src/js/modules/spriteScript.js ***!
+  \****************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"spriteScript\": () => (/* binding */ spriteScript)\n/* harmony export */ });\nclass spriteScript {\r\n    threeMesh\r\n    load\r\n    animate\r\n\r\n    constructor(\r\n        threeMesh,\r\n        load = () => {}, \r\n        animate = () => {}){\r\n        this.threeMesh = threeMesh\r\n        this.load = load\r\n        this.animate = animate\r\n    }\r\n}\r\n\r\n\n\n//# sourceURL=webpack://threejs-webpack-express/./src/js/modules/spriteScript.js?");
 
 /***/ })
 
